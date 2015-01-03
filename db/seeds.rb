58c4383bc3f2479ac64498961246f92b3f0e04b0
Marine Mesnage <marine.mesnage@gmail.com>
2015-01-03 22:50:02 +0100

    clear database

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,3 @@ User.create!(name:  "Admin",
              password_confirmation: "superadmin",
              admin: true)
 
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@test.com"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
